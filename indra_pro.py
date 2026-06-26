857dcfd41e319b602f0f5ec016e997f5fe0f8f57
knierzy <42247124+knierzy@users.noreply.github.com>
2026-06-26 16:25:17 +0200

    Update indra_pro.py

--- indra_pro.py
+++ indra_pro.py
@@ -2366,13 +2366,20 @@ try:
         hoverlabel=dict(font_size=16),
         plot_bgcolor="white"
     )
-    st.plotly_chart(
-        fig,
-        use_container_width=True,
+    html = fig.to_html(
+        include_plotlyjs="cdn",
+        full_html=False,
         config={
+            "responsive": False,
             "displaylogo": False,
             "modeBarButtonsToRemove": ["toImage"]
         }
+    )
+
+    components.html(
+        html,
+        height=750,
+        scrolling=True
     )
    
     from io import BytesIO
