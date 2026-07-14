c92d53ec994204dd3c3545bc3bed00ce255596c0
knierzy <42247124+knierzy@users.noreply.github.com>
2026-07-14 15:44:14 +0200

    Update indra_pro.py

--- indra_pro.py
+++ indra_pro.py
@@ -1864,10 +1864,10 @@ try:
 
     custom_scale = sorted(custom_scale, key=lambda z: z[0])
 
-if color_scale_choice == "Custom INDRA (nichtlinear)":
-    active_colorscale = custom_scale
-else:
-    active_colorscale = color_scale_choice
+    if color_scale_choice == "Custom INDRA (nichtlinear)":
+        active_colorscale = custom_scale
+    else:
+        active_colorscale = color_scale_choice
 
   
     # ============================================================
